9b1e8fce95ae4cc1f8aa30f7cd2594b73c4c8b75
jonmejia <jonathan@dataminded.net>
2024-03-10 20:45:36 -0400

    feat: added nvim conf

---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,31 @@
+---@diagnostic disable: undefined-global
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver", "yamlls", "prismals", "marksman" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.yamlls.setup({})
+			lspconfig.prismals.setup({})
+			lspconfig.marksman.setup({})
+
+			vim.keymap.set("n", "?", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
---
+++ nvim/lua/vim-config.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_exec([[set number]], false)
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+vim.g.mapleader = " "
